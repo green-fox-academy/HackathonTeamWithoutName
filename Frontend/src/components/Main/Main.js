655ecd262d6319d95f402aa9b8694c0a5779a621
Viktor Bertalan <bertalan.viktor@gmail.com>
2021-04-27 17:41:52 +0200

    cart components created, without functonality, re-style needed

--- Frontend/src/components/Main/Main.js
+++ Frontend/src/components/Main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { Main1, Upload } from '../';
+import { Main1, Upload, Cart } from '../';
 
 export const Main = () => {
 
@@ -9,6 +9,7 @@ export const Main = () => {
       <Switch>
         <Redirect exact from="/main" to="/main/main1" />
         <Route path="/main/main1" component={Main1} />
+        <Route path="/main/cart" component={Cart} />
         <Route path="/main/upload" component={Upload} />
       </Switch>
     </div>
