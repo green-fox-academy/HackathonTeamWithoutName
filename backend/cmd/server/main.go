05ab2557b254338661570386b8d865ee9827618f
donatmolnar <donatmolnar@gmail.com>
2021-04-29 00:14:27 +0200

    PUT /order and POST /order finished

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"log"
 
+	"coffeeShop/cmd/addOrder"
+	"coffeeShop/cmd/sendOrder"
+	"coffeeShop/cmd/changePass"
+	"coffeeShop/cmd/createAddress"
+	"coffeeShop/cmd/deleteOrder"
 	"coffeeShop/cmd/forgottenPass"
 	"coffeeShop/cmd/login"
 	"coffeeShop/cmd/postReview"
 	"coffeeShop/cmd/productFeed"
 	"coffeeShop/cmd/register"
+	"coffeeShop/cmd/updateAddress"
+	"coffeeShop/cmd/updateQuantity"
 	"coffeeShop/cmd/verify"
-	"coffeeShop/cmd/changePass"
-	"coffeeShop/cmd/addOrder"
-	"coffeeShop/cmd/sendOrder"
 	"coffeeShop/internal/corsMiddle"
 
 	"github.com/gin-gonic/contrib/static"
@@ -24,19 +28,18 @@ func main() {
 	router.Use(static.Serve("/", static.LocalFile("../../../Frontend/public", true)))
 	router.GET("/user/verify", verify.VerifyUserByEmail)
 	router.POST("/user/forgottenpass", forgottenPass.ForgottenPass)
-
 	router.POST("/user/register", register.RegisterTheUser)
 	router.POST("/user/login", login.LoginFunction)
-	// router.POST("/user/address", )
-	// router.PUT("/user/address", )
+	router.POST("/user/address", createAddress.CreateAddress)
+	router.PUT("/user/address", updateAddress.UpdateAddress)
 	router.PUT("/user/pass", changePass.ChangePass)
 
 	router.GET("/product", productFeed.GetAllProducts)
 	router.POST("/product/review", postReview.PostReview)
-	
+
 	router.POST("/order", addOrder.AddOrder)
 	router.PUT("/order", sendOrder.SendOrder)
-	// router.PUT("/order/quantity",)
-	// router.DELETE("/order", deleteOrder.DeleteOrder)
+	router.PUT("/order/quantity", updateQuantity.UpdateQuantity)
+	router.DELETE("/order", deleteOrder.DeleteOrder)
 	log.Fatal(router.Run(":8080"))
 }
