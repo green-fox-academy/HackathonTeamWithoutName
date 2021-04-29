21d8ca99ae96f27b3e136cdad303e85d96cad0a9
Kocsis Máté <73794121+R4zyl@users.noreply.github.com>
2021-04-29 11:09:31 +0200

    landing page setup

---
+++ Frontend/src/components/LandingPage/CoffeeAnimation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../styles/LandingPage.css';
+
+export const CoffeeAnimation =() => {
+    return (
+<div class="container">
+  <div class="row beans">
+  </div>
+  <div class="row grinder">
+  </div>
+  <div class="row grinder_bottom">
+  </div>
+  <div class="row v60">
+  </div>
+  <div class="row drip">
+  </div>
+  <div class="row cup">
+  </div>
+</div>
+    )
+}
--- Frontend/src/components/LandingPage/LandingPage.js
+++ Frontend/src/components/LandingPage/LandingPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import '../../styles/LandingPage.css';
 
 export const LandingPage = () => {
 
     return (
         <div>
             <div className="section1">
-
+            <div className="landingTitleBox">
+            <div className="landingTitle"><p>Welcome to Coffee to Go</p></div>
+            <div className="landingText"><p>Everything and more about the favourite beverage of the world</p></div>
+            </div>
             </div>
             <div className="section2">
 
